77e0e3156ed18366e4cede924b4bca4a000bdcf5
collin <barnwell.collin@gmail.com>
2016-02-19 16:50:04 -0600

    replaced ORD w CHI, fixed class methods

--- price-getter.rb
+++ price-getter.rb
@@ -47,77 +47,96 @@ class PriceTracker
     @headless = Headless.new
     @headless.start
     @browser = Watir::Browser.new
+
     @@host = "https://www.kayak.com/flights/"
   end
 
+  # def self.collect_data_mt
+  #   pool = Pool.new
+  #   threads = []
+  #   5.times do |i|
+  #     Thread.new {
+  #       pt = PriceTracker.new
+  #       while true
+  #         pool.sem.synchronize {
+  #           f = pool.flights.shift
+  #         }
+  #         break unless f
+  #         pt.write_price(f, pool.sem, pool.flights)
+  #       end
+  #     }
+  #   end
+  #   threads.each {|t| t.join}
+  # end
+
+  # def write_price(f, sem, flights)
+  #   if price = self.get_price_today(f)
+  #     open("output/#{transform_flight_name(f)}.csv", 'a') do |f|
+  #       f.puts "#{date},#{price}"
+  #     end
+  #   else
+  #     sem.synchronize {
+  #       flights.push(f)
+  #     }
+  #   end
+  # end
+
   def self.collect_data
+    fail_count = 0
     flights = Pool.new.flights
     pt = PriceTracker.new
     flights.each do |f|
       price = nil
       while !price
-        begin
-          price = pt.get_price_today(f)
-        rescue
+        price = pt.get_price_today(f)
+        unless price
           print 'f'
+          fail_count+=1
+          throw :kayak_suspects_something if fail_count > 40
         end
       end
       open("output/#{pt.transform_flight_name(f)}.csv", 'a') do |f|
-        f.puts "#{date},#{price}"
+        f.puts "#{pt.date},#{price}"
       end
-    end
-  end
-
-  def self.collect_data_mt
-    pool = Pool.new
-    threads = []
-    5.times do |i|
-      Thread.new {
-        pt = PriceTracker.new
-        while true
-          pool.sem.synchronize {
-            f = pool.flights.shift
-          }
-          break unless f
-          pt.write_price(f, pool.sem, pool.flights)
-        end
-      }
-    end
-    threads.each {|t| t.join}
-  end
-
-  def write_price(f, sem, flights)
-    if price = self.get_price_today(f)
-      open("output/#{transform_flight_name(f)}.csv", 'a') do |f|
-        f.puts "#{date},#{price}"
-      end
-    else
-      sem.synchronize {
-        flights.push(f)
-      }
     end
   end
 
   def get_price_today(flight)
-    @browser.goto(@@host + flight)
-    file_title = "#{transform_flight_name(flight)} #{date}.html"
-
     begin
-      open('html/' + file_title, "w"){ |f| f.puts @browser.html }
+      @browser.goto(@@host + flight)
     rescue
       return nil
     end
 
-    page = Nokogiri::HTML(open('html/' + file_title))
+    file_title = "#{transform_flight_name(flight)} #{date}.html"
+    open('html/' + file_title, "w"){ |f| f.puts @browser.html }
+
+    begin
+      page = Nokogiri::HTML(open('html/' + file_title))
+    rescue
+      return nil
+    end
+
+    # if page.css('div.recaptcha-checkbox-checkmark')[0]
+    #   print 'c'
+    #   @browser.div(class: 'recaptcha-checkbox-checkmark').fire_event :click
+    #   open('html/' + file_title, "w"){ |f| f.puts @browser.html }
+    #   begin
+    #     page = Nokogiri::HTML(open('html/' + file_title))
+    #   rescue
+    #     return nil
+    #   end
+    # end
+
     print '.'
     return page.css('#content_div a.results_price')[0].text.gsub('$', '')
   end
 
-  def self.date
+  def date
     Time.new.strftime("%Y-%m-%d")
   end
 
-  def self.transform_flight_name(fl)
+  def transform_flight_name(fl)
     fl.gsub('/', '-')
   end
 end
--- scrape-prices.rb
+++ scrape-prices.rb
@@ -1,3 +1,3 @@
-require 'price-getter.rb'
+require '~/apps/flight-price/price-getter.rb'
 
 PriceTracker.collect_data
